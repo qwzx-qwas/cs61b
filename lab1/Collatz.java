26510cb2ec0f4d19803f51a3f97bc927ef7ed206
qwzx-qwas <1913840946@qq.com>
2025-05-20 22:42:42 +0800

    code

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,17 +2,17 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-     /**Buggy implementation of nextNumber!;*/
+    /**Buggy implementation of nextNumber!;*/
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        } else if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
-    
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
