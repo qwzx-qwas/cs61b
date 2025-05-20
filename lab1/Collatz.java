5dfce50b9e35d3c1f5bcaf5c5ce455266f0a93cf
qwzx-qwas <1913840946@qq.com>
2025-05-20 21:17:00 +0800

    error code

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -12,14 +12,23 @@ public class Collatz {
             return n * 2;
         }
     } */
-    public static int nextNumber(int n){
+    /*public static int nextNumber(int n){
         if (n % 2 == 0) {
             return n / 2;
         }
         else{
             return n * 3 + 1;
         }
+        }*/
+    public static int nextNumber(int n) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
+    }
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
