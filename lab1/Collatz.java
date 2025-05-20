46ccb372ae14753416fd397921f4556ee12ebadd
qwzx-qwas <1913840946@qq.com>
2025-05-20 21:31:34 +0800

    error code

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,24 +2,7 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-    /* Buggy implementation of nextNumber!
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    } */
-    /*public static int nextNumber(int n){
-        if (n % 2 == 0) {
-            return n / 2;
-        }
-        else{
-            return n * 3 + 1;
-        }
-        }*/
+     /**Buggy implementation of nextNumber!;*/
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
@@ -29,6 +12,7 @@ public class Collatz {
             return n * 2;
         }
     }
+    
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
